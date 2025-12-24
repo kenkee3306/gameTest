b9d1d1b6f549d9d95d558e2bb88e619df59a3723
KenKee-Agmo <kenkee@agmostudio.com>
2025-12-24 12:16:21 +0800

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Todak Studios Sdn Bhd-Word Rice-0.2c";
+const contentToCache = [
+    "Build/PWA V0.2c.loader.js",
+    "Build/PWA V0.2c.framework.js.br",
+    "Build/PWA V0.2c.data.br",
+    "Build/PWA V0.2c.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
